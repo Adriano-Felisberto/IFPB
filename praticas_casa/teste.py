46721b465184ee1345070b856d78304fd863ca11
Adriano Felisberto <146196739+Adriano-Felisberto@users.noreply.github.com>
2025-09-24 15:47:42 -0300

    Delete praticas_casa directory

--- praticas_casa/teste.py
+++
@@ -1,9 +0,0 @@
-while True:
-    nome = str(input("Digite seu nome: ")).upper()
-    
-    if nome == "ADRIANO":
-        print("Olá {}".format(nome))
-        print("Olá mundo")
-        break
-    else:
-        print("Sai daqui vagabundo")
